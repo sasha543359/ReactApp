e100720ba41a3dff0c8312f5ebbc57a2f64b2a97
Alejandro Alejandrino <sasha543359@gmail.com>
2022-07-07 06:05:24 +0300

    Added css modules

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import s from './Header.module.css'
 
 function Header() {
     return(
-        <div class="header">
-  <a href="#default" class="logo"><img width={100} height={50} src="https://upload.wikimedia.org/wikipedia/commons/a/ab/Meta-Logo.png"/></a>
-  <div class="header-right">
-    <a class="active" href="#home"><img width={20} height={20} src="https://www.pngall.com/wp-content/uploads/4/Settings-PNG-Images.png"></img></a>
+        <div class={s.header}>
+  <a href="#default" class={s.logo}><img width={100} height={50} src="https://upload.wikimedia.org/wikipedia/commons/a/ab/Meta-Logo.png"/></a>
+  <div class={s.headerRight}>
+    <a class={s.active} href="#home"><img width={20} height={20} src="https://www.pngall.com/wp-content/uploads/4/Settings-PNG-Images.png"></img></a>
     
     <a href="#about">LogOut</a>
   </div>
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import s from './Nav.module.css'
 
 function Navbar() {
     return (
-        <ul className='Navigation'>
-  <li className="NavigationLi"><a className="NavigationA" href="default.asp">Profile</a></li>
-  <li className="NavigationLi"><a className="NavigationA" href="news.asp">Messages</a></li>
-  <li className="NavigationLi"><a className="NavigationA" href="contact.asp">News</a></li>
-  <li className="NavigationLi"><a className="NavigationA" href="about.asp">Music</a></li>
+        <ul className={s.Navigation}>
+  <li className={s.NavigationLi}><a className={s.NavigationA} href="default.asp">Profile</a></li>
+  <li className={s.NavigationLi}><a className={s.NavigationA} href="news.asp">Messages</a></li>
+  <li className={s.NavigationLi}><a className={s.NavigationA} href="contact.asp">News</a></li>
+  <li className={s.NavigationLi}><a className={s.NavigationA} href="about.asp">Music</a></li>
 </ul>
     );
 }
